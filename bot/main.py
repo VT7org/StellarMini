c676bd50e124e19256054a6113889e6c11b3c7e2
VT7org <glitchnaxusofficial@gmail.com>
2025-04-14 15:20:35 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
