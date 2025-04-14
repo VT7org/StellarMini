a7fa00da2fd69a105d13095639a27f9a02917f52
VT7org <glitchnaxusofficial@gmail.com>
2025-04-14 13:40:18 +0530

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from jinja2 import Template
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/play", response_class=HTMLResponse)
+async def play(id: str, title: str, artist: str):
+    with open("templates/player.html") as f:
+        template = Template(f.read())
+    return template.render(video_id=id, title=title, artist=artist)
