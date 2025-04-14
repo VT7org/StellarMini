98421eaef20b700d87bcef671fae2e2f177637d6
VT7org <glitchnaxusofficial@gmail.com>
2025-04-14 15:17:07 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,13 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 from dotenv import load_dotenv
-from bot.utils.queue import queue
-from bot.utils.mongo import init_mongo, is_admin
-from bot.sources.youtube import search_youtube
+from utils.queue import queue
+from utils.mongo import init_mongo, is_admin
+from sources.youtube import search_youtube
 
 load_dotenv()
 bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
